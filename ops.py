90df102eb0bdda5811b9e5a66a648a3b31123d8f
Fen Dweller <chemicalcrux@gmail.com>
2024-05-26 13:11:14 -0500

    Add operator descriptions

--- ops.py
+++ ops.py
@@ -14,6 +14,7 @@ from .export import perform_export
 class UV_Link_Libraries(bpy.types.Operator):
     bl_idname = "attribute_exporter.link_library"
     bl_label = "Link Library"
+    bl_description = "Link data that the add-on depends on."
 
     def execute(self, context: bpy.types.Context):
         with bpy.data.libraries.load(library.BLEND_PATH, link=True) as (data_from, data_to):
@@ -26,6 +27,7 @@ class UV_Link_Libraries(bpy.types.Operator):
 class UV_Export(bpy.types.Operator):
     bl_idname = "attribute_exporter.export"
     bl_label = "Export Data"
+    bl_description = "Export the currently selected package."
 
     def execute(self, context: bpy.types.Context):
         package = ui.get_current_package(context)
@@ -41,6 +43,7 @@ class UV_Export(bpy.types.Operator):
 class UV_Export_All(bpy.types.Operator):
     bl_idname = "attribute_exporter.export_all"
     bl_label = "Export All Data"
+    bl_description = "Export every package."
 
     def execute(self, context: bpy.types.Context):
         for package in context.scene.attribute_exporter_packages:
@@ -52,6 +55,7 @@ class UV_Export_All(bpy.types.Operator):
 class UV_Refresh(bpy.types.Operator):
     bl_idname = "attribute_exporter.refresh"
     bl_label = "Refresh Attribute Choices"
+    bl_description = "Re-calculate the list of attributes you can pick from."
 
     def execute(self, context: bpy.types.Context):
         props.refresh_attribute_choices(self, context)
